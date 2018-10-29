2b4a74b3d11fd5b3749a981db85a7cdaa200d329
Revath S Kumar <rsk@revathskumar.com>
2018-10-30 00:50:37 +0530

    add query type to fetch single record

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -14,5 +14,20 @@ module Types
           g.from_json(user.to_json)
         end
       }
+
+      field :user, UserType,
+        null: false,
+        resolve: -> (obj, args, ctx) {
+          file = File.read File.join(File.dirname(__FILE__), "../data/users.json")
+          data = JSON.parse(file)
+          selectedUser = data.select do |user|
+            user['id'].to_i == args[:id]
+          end.first
+          user = User.new
+          user.from_json(selectedUser.to_json)
+          
+        } do
+          argument :id, Int, required: true
+    end
   end
 end
